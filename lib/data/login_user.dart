1916f642c76a31616682d94a46b6b0df5f678ed9
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-05-05 10:13:20 -0300

    Mapped the user data after login.

--- lib/data/login_user.dart
+++ lib/data/login_user.dart
@@ -62,14 +62,23 @@ class LoginModel {
   });
 
   LoginModel.fromJson(Map<String, dynamic> json) {
+    id = json['_id'];
+    firstName = json['first_name'];
+    lastName = json['last_name'];
     email = json['email'];
     password = json['password'];
+    userToken = json['token'];
+
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = Map<String, dynamic>();
+    data['_id'] = id;
+    data['first_name'] = firstName;
+    data['last_name'] = lastName;
     data['email'] = email;
     data['password'] = password;
+    data['token'] = userToken;
     return data;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(title: "teste"),
+      home: LoginScreen(),
     );
   }
 }
