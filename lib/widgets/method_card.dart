0d8739803b819a44314c4a0b78cf5b917d4ec4fc
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-05-09 19:23:17 -0300

    Saving generic firebase ok

--- lib/widgets/method_card.dart
+++ lib/widgets/method_card.dart
@@ -1,4 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:favorite_button/favorite_button.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -17,6 +18,14 @@ class MethodCard extends StatefulWidget {
 }
 
 class _MethodCardState extends State<MethodCard> {
+  final dataBase = FirebaseFirestore.instance;
+  // test variable - firestore
+  final user = <String, dynamic>{
+    "first": "Ada",
+    "last": "Lovelace",
+    "born": 1815
+  };
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -46,8 +55,13 @@ class _MethodCardState extends State<MethodCard> {
                   iconSize: 50,
                   isFavorite: false,
                   iconColor: Colors.deepPurple,
-                  valueChanged: (_isFavorite) {
-                    print('Is Favorite : $_isFavorite');
+                  valueChanged: (_isFavorite) async {
+                    [
+                      print('Is Favorite : $_isFavorite'),
+                      await dataBase.collection("teste").add(user).then(
+                          (DocumentReference doc) => print(
+                              'DocumentSnapshot added with ID: ${doc.id}'))
+                    ];
                   },
                 ),
               ],
@@ -68,15 +82,16 @@ class _MethodCardState extends State<MethodCard> {
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   return ListView.builder(
-                    itemCount: snapshot.data!.recipes!.secao![0].conteudo!.length,
+                    itemCount:
+                        snapshot.data!.recipes!.secao![0].conteudo!.length,
                     physics: const NeverScrollableScrollPhysics(),
                     shrinkWrap: true,
                     itemBuilder: (context, index) {
                       return Card(
                         elevation: 0,
                         child: ListTile(
-                          title: Text(
-                              snapshot.data!.recipes!.secao![0].conteudo![index]),
+                          title: Text(snapshot
+                              .data!.recipes!.secao![0].conteudo![index]),
                           tileColor: MyColors.primarylight,
                           leading: Container(
                             height: 32,
