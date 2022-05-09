a8804b58739c3e5b82770f40a07dbf437ef3ed70
LaisBonin <101892965+LaisBonin@users.noreply.github.com>
2022-05-09 11:25:46 -0300

    Add Favorite Button

--- lib/widgets/ingredient_card.dart
+++ lib/widgets/ingredient_card.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:favorite_button/favorite_button.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -19,41 +21,51 @@ class IngredientCard extends StatelessWidget {
           children: [
             Container(
               alignment: Alignment.topLeft,
-              child: Text(
+              child: AutoSizeText(
                 "Prato",
                 style: TextStyle(
                   color: MyColors.primarydark,
                   fontFamily: GoogleFonts.ptSerif().fontFamily,
                   fontSize: 24,
                 ),
+                maxLines: 1,
               ),
             ),
             Container(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Modo de Preparo:",
-                style: TextStyle(
-                  color: MyColors.primarydark,
-                  fontFamily: GoogleFonts.ptSerif().fontFamily,
-                  fontSize: 12,
-                ),
-              ),
-            ),
-            ListView(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              children: const <Widget>[
-                Card(
-                  elevation: 0,
-                  child: ListTile(
-                    title: Text(
-                      "1 - Abra o abacaxi no meio, no sentido da coroa para a base. Retire a polpa e cortar em cubos.   2- Abra o abacaxi no meio, no sentido da coroa para a base. Retire a polpa e cortar em cubos. ",
-                      textAlign: TextAlign.center,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Modo de Preparo:",
+                      style: TextStyle(
+                        color: MyColors.primarydark,
+                        fontFamily: GoogleFonts.ptSerif().fontFamily,
+                        fontSize: 12,
+                      ),
                     ),
-                    tileColor: MyColors.primarylight,
-                                    ),
-                ),
-              ],
+                  ),
+                  ListView(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    children: const <Widget>[
+                      Card(
+                        elevation: 0,
+                        child: ListTile(
+                          title: Text(
+                            "1 - Abra o abacaxi no meio, no sentido da coroa para a base. Retire a polpa e cortar em cubos.   2- Abra o abacaxi no meio, no sentido da coroa para a base. Retire a polpa e cortar em cubos. ",
+                            textAlign: TextAlign.center,
+                          ),
+                          tileColor: MyColors.primarylight,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
