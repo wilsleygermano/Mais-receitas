c03b766ddec8ccf4ea7b788ad1b53ae3faa72270
LaisBonin <101892965+LaisBonin@users.noreply.github.com>
2022-05-10 14:59:39 -0300

    Add Page Viewer

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -40,7 +40,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      bottomNavigationBar: BottomBar(),
+     
       body: FutureBuilder(
         future: homeController.start(),
         builder: ((context, snapshot) {
