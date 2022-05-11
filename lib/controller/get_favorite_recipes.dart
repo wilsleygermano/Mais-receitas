0f63065e8cac1fe3c185fbf5e7c0b0add372fa27
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-05-11 09:15:29 -0300

    Update get_favorite_recipes.dart

--- lib/controller/get_favorite_recipes.dart
+++ lib/controller/get_favorite_recipes.dart
@@ -2,9 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future getFavoriteRecipes(String userId) async {
   final dataBase = FirebaseFirestore.instance;
-  final docRef = dataBase.collection(userId);
+  final docRef = dataBase.collection("users").doc(userId).collection("favorite_recipes").doc();
   docRef.get().then(
-        (res) => print("Successfully completed ${res.toString()}"),
+        (res) => print("Successfully completed"),
         onError: (e) => print("Error completing: $e"),
       );
 }
+
+// TODO: try to retrieve firebase documents and build a list
