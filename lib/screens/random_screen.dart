38690fba8ba15b6d70303f2d69c008733e2e61f5
LaisBonin <101892965+LaisBonin@users.noreply.github.com>
2022-05-11 13:07:58 -0300

    Settings

--- lib/screens/random_screen.dart
+++ lib/screens/random_screen.dart
@@ -4,12 +4,56 @@ import 'package:mais_receitas/screens/login_screen.dart';
 import 'package:mais_receitas/widgets/main_button.dart';
 import 'package:mais_receitas/widgets/page_viewer.dart';
 
+import '../controller/home_controller.dart';
 
-
-class RandomScreen extends StatelessWidget {
-  
-
+class RandomScreen extends StatefulWidget {
   const RandomScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RandomScreen> createState() => _RandomScreenState();
+}
+
+final PageController controller = PageController(
+  initialPage: 0,
+  keepPage: true,
+);
+final homeController = HomeController();
+final loading = ValueNotifier(true);
+String randomRecipeName = "";
+int _page = 0;
+
+class _RandomScreenState extends State<RandomScreen> {
+  Widget buildPageView() {
+    return PageView(
+      controller: controller,
+      onPageChanged: (index) {
+        pageChanged(index);
+      },
+    );
+  }
+
+  void pageChanged(int index) {
+    setState(
+      () {
+        if (index == 1) {
+          homeController.allRecipesName.recipesName!.shuffle();
+          randomRecipeName = homeController.allRecipesName.recipesName![0];
+        }
+        _page = index;
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    setState(() {
+      homeController.start();
+      homeController.allRecipesName.recipesName!.shuffle();
+      randomRecipeName = homeController.allRecipesName.recipesName![0];
+      // loadReceitas();
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,13 +97,12 @@ class RandomScreen extends StatelessWidget {
                           alignment: Alignment.center,
                           decoration: const BoxDecoration(
                               image: DecorationImage(
-                            image: AssetImage('lib/images/beet.gif'),
+                            image: AssetImage('lib/images/random.gif'),
                             fit: BoxFit.fill,
                           )),
                         ),
                       ],
                     ),
-                   
                   ],
                 ),
               ),
