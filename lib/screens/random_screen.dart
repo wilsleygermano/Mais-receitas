50e04ec3a5813901a3ed942c51334ac54313f1df
LaisBonin <101892965+LaisBonin@users.noreply.github.com>
2022-05-11 15:00:21 -0300

    Add RandomScreen

--- lib/screens/random_screen.dart
+++ lib/screens/random_screen.dart
@@ -1,71 +1,145 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mais_receitas/controller/home_controller.dart';
+import 'package:mais_receitas/data/user_model.dart';
 import 'package:mais_receitas/screens/login_screen.dart';
+import 'package:mais_receitas/screens/recipes_screen.dart';
 import 'package:mais_receitas/widgets/main_button.dart';
 import 'package:mais_receitas/widgets/page_viewer.dart';
 
+class RandomScreen extends StatefulWidget {
+  final UserModel user;
 
+  const RandomScreen({Key? key, required this.user}) : super(key: key);
 
-class RandomScreen extends StatelessWidget {
-  
+  @override
+  State<RandomScreen> createState() => _RandomScreenState();
+}
 
-  const RandomScreen({Key? key}) : super(key: key);
+class _RandomScreenState extends State<RandomScreen> {
+  final homeController = HomeController();
+  final loading = ValueNotifier(true);
+
+  @override
+  void initState() {
+    setState(() {
+      homeController.start();
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: SafeArea(
-        child: Center(
-          child: Stack(
-            children: [
-              Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('lib/images/background.png'),
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                child: Column(
+      body: FutureBuilder(
+        future: homeController.start(),
+        builder: ((context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return SafeArea(
+              child: Center(
+                child: Stack(
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          height: 268,
-                          width: 237,
-                          alignment: Alignment.topCenter,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('lib/images/logo.png'),
-                              fit: BoxFit.fill,
-                            ),
+                    Container(
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('lib/images/background.png'),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 268,
+                                width: 237,
+                                alignment: Alignment.topCenter,
+                                decoration: const BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage('lib/images/logo.png'),
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height:300,
+                                width: 300,
+                                alignment: Alignment.center,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                  image: AssetImage('lib/images/random.gif'),
+                                  fit: BoxFit.fill,
+                                )),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              MainButton(
+                                labelText: "CLIQUE AQUI",
+                                buttonPressed: () {
+                                  homeController.allRecipesName.recipesName!
+                                      .shuffle();
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: ((context) => RecipesScreen(
+                                            recipesName: homeController
+                                                .allRecipesName.recipesName![0],
+                                          )),
+                                    ),
+                                  );
+                                },
+                              )
+                            ],
+                          )
+                        ],
+                      ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          height: 300,
-                          width: 300,
-                          alignment: Alignment.center,
-                          decoration: const BoxDecoration(
-                              image: DecorationImage(
-                            image: AssetImage('lib/images/random.gif'),
-                            fit: BoxFit.fill,
-                          )),
-                        ),
-                      ],
-                    ),
-                   
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.none) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return Dialog(
+                  backgroundColor: Colors.white,
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: ElevatedButton(
+                    child: const Text("Try again"),
+                    onPressed: () {
+                      setState(
+                        () {
+                          homeController.start();
+                        },
+                      );
+                    },
+                  ),
+                );
+              },
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.purple,
+              strokeWidth: 8.0,
+            ),
+          );
+        }),
       ),
     );
   }
