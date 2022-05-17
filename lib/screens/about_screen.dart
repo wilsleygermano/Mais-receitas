2bcaa64324e6d25de03bcbc9c8939b67fb21d141
Lais Bonin <boninlaisc@gmail.com>
2022-05-17 00:35:07 -0300

    Finishing about screen

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -1,12 +1,10 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mais_receitas/controller/home_controller.dart';
-import 'package:mais_receitas/data/user_model.dart';
 import 'package:mais_receitas/design/my_colors.dart';
-import 'package:mais_receitas/screens/login_screen.dart';
-import 'package:mais_receitas/screens/recipes_screen.dart';
+import 'package:mais_receitas/widgets/return_button.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutScreen extends StatefulWidget {
   const AboutScreen({
@@ -18,10 +16,6 @@ class AboutScreen extends StatefulWidget {
 }
 
 class _AboutScreenState extends State<AboutScreen> {
-  
-
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +23,6 @@ class _AboutScreenState extends State<AboutScreen> {
       body: SafeArea(
         top: false,
         child: FutureBuilder(
-          
           builder: ((context, snapshot) {
             {
               return Center(
@@ -44,6 +37,13 @@ class _AboutScreenState extends State<AboutScreen> {
                       ),
                       child: Column(
                         children: [
+                          const SizedBox(height: 24,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: const [
+                              ReturnButton(),
+                            ],
+                          ),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
@@ -83,43 +83,125 @@ class _AboutScreenState extends State<AboutScreen> {
                                   width: 360,
                                   height: 400,
                                   decoration: BoxDecoration(
-                                      color: MyColors.primaryoriginal.withOpacity(0.1),
-                                      borderRadius: BorderRadius.circular(16.0),
-                                      border: Border.all(
-                                        width: 4.0,
-                                        color: MyColors.primaryoriginal.withOpacity(0.5),
-                                      ),),
+                                    color: MyColors.primaryoriginal
+                                        .withOpacity(0.1),
+                                    borderRadius: BorderRadius.circular(16.0),
+                                    border: Border.all(
+                                      width: 4.0,
+                                      color: MyColors.primaryoriginal
+                                          .withOpacity(0.5),
+                                    ),
+                                  ),
                                   child: Column(
                                     children: [
                                       Text(
                                         " Sobre este projeto:",
                                         style: TextStyle(
-                                            fontFamily:
-                                                GoogleFonts.secularOne()
-                                                    .fontFamily,
+                                            fontFamily: GoogleFonts.secularOne()
+                                                .fontFamily,
                                             fontWeight: FontWeight.bold,
                                             fontSize: 24,
-                                            color: MyColors.primarydark),
+                                            color: const Color.fromARGB(
+                                                255, 82, 70, 114)),
                                       ),
-                                      const SizedBox(height: 16,),
+                                      const SizedBox(
+                                        height: 16,
+                                      ),
                                       Text(
                                         "Elaborado com amor e carinho para a SnowAcademy. ",
+                                        textAlign: TextAlign.center,
                                         style: TextStyle(
-                                            fontFamily:
-                                                GoogleFonts.secularOne()
-                                                    .fontFamily,
+                                            fontFamily: GoogleFonts.secularOne()
+                                                .fontFamily,
                                             fontWeight: FontWeight.w400,
                                             fontSize: 20,
                                             color: MyColors.primarydark),
                                       ),
-                                      
+                                      const SizedBox(
+                                        height: 42,
+                                      ),
+                                      Text(
+                                        " Conheça mais sobre o desenvolvimento:",
+                                        style: TextStyle(
+                                            fontFamily: GoogleFonts.secularOne()
+                                                .fontFamily,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16,
+                                            color: const Color.fromARGB(
+                                                255, 82, 70, 114)),
+                                      ),
+                                      const SizedBox(height: 24),
+                                      ElevatedButton.icon(
+                                        style: ButtonStyle(
+                                          backgroundColor:
+                                              MaterialStateProperty.all<Color>(
+                                                  MyColors.primaryoriginal),
+                                          shape: MaterialStateProperty.all<
+                                              RoundedRectangleBorder>(
+                                            RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                            ),
+                                          ),
+                                        ),
+                                        onPressed: () => launchUrl(Uri.parse(
+                                            "https://github.com/LaisBonin")),
+                                        icon: Image.asset(
+                                          'lib/images/github_logo.png',
+                                          height: 24,
+                                          width: 24,
+                                        ),
+                                        label: Text(
+                                          'https://github.com/LaisBonin',
+                                          style: TextStyle(
+                                            color: const Color.fromARGB(
+                                                255, 82, 70, 114),
+                                            fontFamily: GoogleFonts.secularOne()
+                                                .fontFamily,
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(height: 12),
+                                      ElevatedButton.icon(
+                                        style: ButtonStyle(
+                                          backgroundColor:
+                                              MaterialStateProperty.all<Color>(
+                                                  MyColors.primaryoriginal),
+                                          shape: MaterialStateProperty.all<
+                                              RoundedRectangleBorder>(
+                                            RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                            ),
+                                          ),
+                                        ),
+                                        onPressed: () => launchUrl(Uri.parse(
+                                            "https://github.com/wilsleygermano")),
+                                        icon: Image.asset(
+                                          'lib/images/github_logo.png',
+                                          height: 24,
+                                          width: 24,
+                                        ),
+                                        label: Text(
+                                          'https://github.com/wilsleygermano',
+                                          style: TextStyle(
+                                            color: const Color.fromARGB(
+                                                255, 82, 70, 114),
+                                            fontFamily: GoogleFonts.secularOne()
+                                                .fontFamily,
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                      ),
                                     ],
                                   ),
                                 ),
                               ),
                             ),
                           ),
-                          
                         ],
                       ),
                     ),
